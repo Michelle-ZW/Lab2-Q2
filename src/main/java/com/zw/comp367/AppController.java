2e1972c3198b59b72d6027f054da064499bf6e0a
Zhenqiao Wang <zwang279@my.centennialcollege.ca>
2024-02-24 08:39:10 -0500

    modified text display

--- src/main/java/com/zw/comp367/AppController.java
+++ src/main/java/com/zw/comp367/AppController.java
@@ -1,14 +1,32 @@
 package com.zw.comp367;
 
 
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
 	@GetMapping("/")
-	public String home()
+	public String home(Model model)
 	{
+		int hour = LocalTime.now().getHour();
+		
+		String message = "";
+		
+		if(hour <= 12 && hour >= 17)
+		{
+			message = "Good afternoon, Zhenqiao, Welcome to COMP367";
+		}
+		else 
+		{
+			message = "Good morning, Zhenqiao, Welcome to COMP367";
+		}
+		
+		model.addAttribute("message", message);
+		
 		return "index";
 	}
 
